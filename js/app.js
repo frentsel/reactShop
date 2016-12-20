a65c23120d0ea71b556d9abe7a16d75432abf5b5
frentsel <frentsel@mail.ru>
2016-12-21 00:40:40 +0200

    show product list

--- js/app.js
+++ js/app.js
@@ -1,17 +1,39 @@
 "use strict";
 
-const products = [1, 2, 3, 4, 5];
+class Product extends React.Component {
+
+	constructor (props) {
+
+		super(props);
+		this.phone = props.phone;
+	}
+
+	handler(e){
+		console.info(e.target);
+	}
+
+	render(){
+		return (
+			<div id={this.phone.id} className="phone" onClick={this.handler}>
+				<strong>{this.phone.name}</strong>
+				<img src={this.phone.imageUrl} alt={this.phone.name}/>
+				{/*<p>{this.phone.snippet}</p>*/}
+			</div>
+		)
+	}
+}
 
 function ProductsList(props){
 
-	const list = props.products.map((product) =>
-		<div>{product}</div>
-	);
-
+	const list = props.products.map((product) => <Product phone={product}/>);
 	return <div>{list}</div>;
 }
 
-ReactDOM.render(
-	<ProductsList products={products} />,
-	document.getElementById('productsList')
-);
+
+$.getJSON('js/phones.json',{},function (products) {
+
+	ReactDOM.render(
+		<ProductsList products={products} />,
+		document.getElementById('productsList')
+	);
+});
