7f9f4ba1ce7fbb0397fbd41443417487a2196cba
Alex Shulz <frentsel@mail.ru>
2017-02-21 11:05:24 +0200

    setup production environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,19 @@
 var webpack = require('webpack');
 
-module.exports = {
+var isProduction = false;
+
+var config = {
     entry: "./react/js/App.jsx",
     output: {
         path: __dirname + '/react/',
         publicPath: "react/",
-        filename: 'bundle.min.js',
+        filename: 'bundle.js',
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false // https://github.com/webpack/webpack/issues/1496
-            }
-        }),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"production"',
-            DEVELOPMENT: false,
-            DEBUG: false,
+            'process.env.NODE_ENV': 'development',
+            DEVELOPMENT: true,
+            DEBUG: true,
         }),
     ],
     watch: true,
@@ -31,27 +28,27 @@ module.exports = {
                 loader: "style-loader!css-loader!autoprefixer-loader",
                 // exclude: [/node_modules/, /react/]
             },
-            {
-                test: /\.less$/,
-                loader: "style-loader!css-loader!autoprefixer-loader!less",
-                // exclude: [/node_modules/, /react/]
-            },
-            {
-                test: /\.gif$/,
-                loader: "url-loader?limit=10000&mimetype=image/gif"
-            },
-            {
-                test: /\.jpg$/,
-                loader: "url-loader?limit=10000&mimetype=image/jpg"
-            },
-            {
-                test: /\.png$/,
-                loader: "url-loader?limit=10000&mimetype=image/png"
-            },
-            {
-                test: /\.svg/,
-                loader: "url-loader?limit=26000&mimetype=image/svg+xml"
-            },
+            // {
+            //     test: /\.less$/,
+            //     loader: "style-loader!css-loader!autoprefixer-loader!less",
+            //     // exclude: [/node_modules/, /react/]
+            // },
+            // {
+            //     test: /\.gif$/,
+            //     loader: "url-loader?limit=10000&mimetype=image/gif"
+            // },
+            // {
+            //     test: /\.jpg$/,
+            //     loader: "url-loader?limit=10000&mimetype=image/jpg"
+            // },
+            // {
+            //     test: /\.png$/,
+            //     loader: "url-loader?limit=10000&mimetype=image/png"
+            // },
+            // {
+            //     test: /\.svg/,
+            //     loader: "url-loader?limit=26000&mimetype=image/svg+xml"
+            // },
             {
                 test: /\.jsx$/,
                 loader: "react-hot!babel",
@@ -60,10 +57,29 @@ module.exports = {
                 //     presets: ["es2015", "react"]
                 // }
             },
-            {
-                test: /\.json$/,
-                loader: "json-loader"
-            }
+            // {
+            //     test: /\.json$/,
+            //     loader: "json-loader"
+            // }
         ]
     }
 };
+
+if (isProduction) {
+
+    config.output.filename = 'bundle.min.js';
+    config.plugins = [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': 'production',
+            DEVELOPMENT: false,
+            DEBUG: false,
+        }),
+    ];
+}
+
+module.exports = config;
